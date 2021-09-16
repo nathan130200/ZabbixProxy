f3e1d408b6f2d9afd3741a0543fb59769743b68c
Nathan Ferreira <14365254+nathan130200@users.noreply.github.com>
2021-09-16 17:56:57 -0300

    Added sample application

---
+++ ZabbixProxy.Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using Zabbix;
+
+var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += (s, e) =>
+{
+    if (!cts.IsCancellationRequested)
+        cts.Cancel();
+
+    e.Cancel = true;
+};
+
+var agent = new PassiveAgent(
+    listenUri: new Uri("tcp://0.0.0.0:10052"),
+    connectUri: new Uri("tcp://0.0.0.0:10050")
+);
+
+await agent.StartAsync();
+
+while (!cts.IsCancellationRequested)
+    await Task.Delay(1);
+
+await agent.StopAsync();
